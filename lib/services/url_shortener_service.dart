6dfbcc684196b75f1017f60d675af7b8b1c9b708
Juan Alegría <juanszalegria@gmail.com>
2024-01-06 11:59:13 -0500

    Feat: create url shortener service

---
+++ lib/services/url_shortener_service.dart
@@ -0,0 +1,19 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class UrlShortenerService {
+  Future<String> shortenUrl(String url) async {
+    var response = await http.post(
+      Uri.parse('https://url-shortener-server.onrender.com/api/alias'),
+      headers: {'Content-Type': 'application/json'},
+      body: json.encode({'url': url}),
+    );
+
+    if (response.statusCode == 201) {
+      var data = json.decode(response.body);
+      return data['short'];
+    } else {
+      throw Exception('Failed to shorten URL');
+    }
+  }
+}
