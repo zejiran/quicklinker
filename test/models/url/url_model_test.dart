9b48131d358dfe82cdc8ea933a8b49e7f6697185
Juan Alegría <juanszalegria@gmail.com>
2024-01-08 16:40:05 -0500

    Test: remove unnecessary setup

--- test/models/url/url_model_test.dart
+++ test/models/url/url_model_test.dart
@@ -1,15 +1,8 @@
-import 'dart:io';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:hive/hive.dart';
 import 'package:quicklinker/models/url/url_model.dart';
 
 void main() {
   group('UrlModel Tests', () {
-    setUpAll(() async {
-      Hive.init('${Directory.current.path}/test/hive_testing_dir');
-      Hive.registerAdapter(UrlModelAdapter());
-    });
-
     test('should correctly instantiate UrlModel', () {
       const String testOriginalUrl = 'https://example.com';
       const String testShortUrl = 'https://short.url';
@@ -24,14 +17,6 @@ void main() {
       expect(urlModel.originalUrl, equals(testOriginalUrl));
       expect(urlModel.shortUrl, equals(testShortUrl));
       expect(urlModel.timestamp, equals(testTimestamp));
-    });
-
-    tearDownAll(() async {
-      await Hive.close();
-      final directory = Directory('${Directory.current.path}/test/hive_testing_dir');
-      if (await directory.exists()) {
-        await directory.delete(recursive: true);
-      }
     });
   });
 }
